7eb35b8c981862923b7aa875613643178b31c0b9
robust <>
2023-02-07 18:57:31 +0800

    add

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*")
