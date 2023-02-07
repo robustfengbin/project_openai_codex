da76dd02882bd6d9f300a7e8ed411ad856a128e0
robust <>
2023-02-07 19:09:01 +0800

    aa

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://gpt.fastaitop.com/api', {
+    const response = await fetch('https://gpt.fastaitop.com/api/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
